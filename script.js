9a505df5588e1603137c38b204c06134df7c4907
Elio <elbar@live.it>
2022-11-06 23:46:31 +0100

    Add reset button and newGame option

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 2 variables to keep track of scores, initialized to 0 to define their type
+// 2 variables to keep track of scores, initialized to 0 to immediately define their type
 let playerScore = 0;
 let computerScore = 0;
 
@@ -10,7 +10,10 @@ let computerChoice;
 const playerDisplayPoints = document.getElementById("playerPoints");    // player points
 const cpuDisplayPoints = document.getElementById("cpuPoints") ;         // cpu points
 const resultText = document.getElementById("resultText");               // paragraph to announce results
-const battleSymbols = document.getElementById("battle-symbols");
+const battleSymbols = document.getElementById("battle-symbols");        // area to put/remove "weapon" images
+const stickmanArea = document.getElementById("battle-stickmen");        // stickman area
+// collection of elements to be hidden after victoryImage or defeat
+const toHide = document.getElementsByClassName("to-hide");
 
 // get references to player buttons
 const plRock = document.getElementById("btnPlRock");
@@ -32,9 +35,9 @@ plScissors.addEventListener("click", (e) => {
     getComputerChoice();
     playRound("scissors", computerChoice);
 });
+//------------------------------------------------------------------------------------------
 
-
-// define playRound
+// define playRound()
 function playRound(playerChoice, computerChoice) {
 
     // remove previous battle icons in the middle, if existing
@@ -72,74 +75,76 @@ function playRound(playerChoice, computerChoice) {
     }
 
     // if statements to create battle icons
-    if (playerChoice === "rock" ) {                         // ROCK situation
+    // can merge with above "if" code for better formatting
+    // each block creates and appends the corresponding "weapon" image
+    if (playerChoice === "rock" ) {                         // player chose ROCK
         const img = document.createElement("img");
         img.src = "img/Rock.png"
-        img.className = "battle-icon"
+        img.className = "battle-icon to-hide"
         battleSymbols.appendChild(img);
 
         if (computerChoice === "rock") {
             const img = document.createElement("img");
             img.src = "img/Rock.png"
-            img.className = "battle-icon-cpu"
+            img.className = "battle-icon-cpu to-hide"
             battleSymbols.appendChild(img);
         } else if (computerChoice === "paper") {
             const img = document.createElement("img");
             img.src = "img/Paper.png"
-            img.className = "battle-icon-cpu"
+            img.className = "battle-icon-cpu to-hide"
             battleSymbols.appendChild(img);
         } else if (computerChoice === "scissors") {
             const img = document.createElement("img");
             img.src = "img/Scissors.png"
-            img.className = "battle-icon-cpu"
+            img.className = "battle-icon-cpu to-hide"
             battleSymbols.appendChild(img);
         }
     }
     
-    if (playerChoice === "paper" ) {                        // PAPER situation
+    if (playerChoice === "paper" ) {                        // player chose PAPER
         const img = document.createElement("img");
         img.src = "img/Paper.png"
-        img.className = "battle-icon"
+        img.className = "battle-icon to-hide"
         battleSymbols.appendChild(img);
  
         if (computerChoice === "rock") {
             const img = document.createElement("img");
             img.src = "img/Rock.png"
-            img.className = "battle-icon-cpu"
+            img.className = "battle-icon-cpu to-hide"
             battleSymbols.appendChild(img);
         } else if (computerChoice === "paper") {
             const img = document.createElement("img");
             img.src = "img/Paper.png"
-            img.className = "battle-icon-cpu"
+            img.className = "battle-icon-cpu to-hide"
             battleSymbols.appendChild(img);
         } else if (computerChoice === "scissors") {
             const img = document.createElement("img");
             img.src = "img/Scissors.png"
-            img.className = "battle-icon-cpu"
+            img.className = "battle-icon-cpu to-hide"
             battleSymbols.appendChild(img);
         }
     }
     
-    if (playerChoice === "scissors" ) {                     // SCISSORS situation
+    if (playerChoice === "scissors" ) {                     // player chose SCISSORS
         const img = document.createElement("img");
         img.src = "img/Scissors.png"
-        img.className = "battle-icon"
+        img.className = "battle-icon to-hide"
         battleSymbols.appendChild(img);
 
         if (computerChoice === "rock") {
             const img = document.createElement("img");
             img.src = "img/Rock.png"
-            img.className = "battle-icon-cpu"
+            img.className = "battle-icon-cpu to-hide"
             battleSymbols.appendChild(img);
         } else if (computerChoice === "paper") {
             const img = document.createElement("img");
             img.src = "img/Paper.png"
-            img.className = "battle-icon-cpu"
+            img.className = "battle-icon-cpu to-hide"
             battleSymbols.appendChild(img);
         } else if (computerChoice === "scissors") {
             const img = document.createElement("img");
             img.src = "img/Scissors.png"
-            img.className = "battle-icon-cpu"
+            img.className = "battle-icon-cpu to-hide"
             battleSymbols.appendChild(img);
         }
     }
@@ -147,12 +152,19 @@ function playRound(playerChoice, computerChoice) {
     //score visual changes
     playerDisplayPoints.textContent = playerScore;
     cpuDisplayPoints.textContent = computerScore;
+
+    // score check to trigger endGame().
+    if( playerScore == 5 || computerScore == 5 ) {
+        endGame();
+    }
+
 }
 
+//------------------------------------------------------------------------------------------
 
-// define getComputerChoice
+// define getComputerChoice()
 function getComputerChoice() {
-    // random integer 1 2 or 3
+    // random integer 1, 2 or 3
     const randomN = Math.floor(Math.random() * 3) + 1;
     // link 1 to rock, 2 to paper, 3 to scissors
     if (randomN == 1) {
@@ -164,3 +176,58 @@ function getComputerChoice() {
     }
     return computerChoice
 }
+
+//------------------------------------------------------------------------------------------
+
+// define endgame(); triggered when any score is 5
+function endGame() {
+    // hide elements with "to-hide" class
+    Array.from(toHide).forEach(element => {
+        element.setAttribute("hidden", true);
+    });
+
+    if ( playerScore > computerScore ) {                    // player is winner
+        resultText.textContent = "YOU WON THE MATCH!";
+        // create winner image and append
+        const victoryImage = document.createElement("img");
+        victoryImage.src = "img/plWin.png";
+        victoryImage.className = "imgResult";
+        battleSymbols.appendChild(victoryImage);
+    } else {                                                // cpu is winner
+        resultText.textContent = "YOU LOST THE MATCH!";
+        // creater loser image and append
+        const lossImage = document.createElement("img");
+        lossImage.src = "img/cpuWin.png";
+        lossImage.className = "imgResult";
+        battleSymbols.appendChild(lossImage);
+    }
+
+    // create and append reset button to start new game
+    const resetBtn = document.createElement("button");
+    resetBtn.textContent = "New Game";
+    // gave same formatting for simplicity
+    resetBtn.className = "btn";
+    stickmanArea.appendChild(resetBtn);
+    // reset button function on "click"
+    resetBtn.addEventListener("click", (e) => {
+        // remove victoryImage/lossImage and 2 previously hidden "weapon" images
+        while (battleSymbols.firstChild) {
+            battleSymbols.removeChild(battleSymbols.firstChild);
+        }    
+        // empty result text
+        resultText.textContent = '';
+        // remove "hidden" status from other elements
+        Array.from(toHide).forEach(element => {
+            element.removeAttribute("hidden");
+        })
+        // reset score
+        playerScore = 0;
+        computerScore = 0;
+        playerDisplayPoints.textContent = playerScore;
+        cpuDisplayPoints.textContent = computerScore;
+
+        //remove itself so it can be used only once per match
+        stickmanArea.removeChild(resetBtn);
+    });
+}
+
